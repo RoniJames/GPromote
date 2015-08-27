42b348f6de24e365c69948932cb32415e0a7c4e8
boyraad300 <boyraad300@yahoo.com>
2015-08-26 22:18:04 -0400

    Script Published
    Publishing the script V1

---
+++ lua/autorun/server/autopromote.lua
@@ -0,0 +1,29 @@
+--Group to start promoting people
+local startinggroup = "member"
+-- Last group in the inheritance tree that should be promoted
+local endinggroup = "tmod"
+--Put any extra groups you dont want being demoted/promoted
+--format = [NEXTNUMBER] = "GROUP", <--- dont forget the comma
+local extrablacklisted = {
+[1] = "vip",
+[2] = "vip+",
+
+}
+-- format = [HOURS] = "RANK", <--- dont forget the comma
+local autopromote = {  
+[24] = "regular",
+[50] = "trusted",
+[80] = "veteran",
+
+}
+
+timer.Create( "autopromotev2", 10, 0, function()
+	for k,v in pairs( player.GetAll() ) do
+	local uptime = v:GetUTimeTotalTime() / 3600
+		for t,r in pairs( autopromote ) do		
+			if uptime >= t and v:CheckGroup( startinggroup ) and !v:CheckGroup( endinggroup ) and !table.HasValue(extrablacklisted,v:GetUserGroup()) and !v:IsAdmin() and !v:CheckGroup( r ) then
+				RunConsoleCommand( "ulx","adduser", v:Nick(), r )
+			end
+		end
+	end
+end )
